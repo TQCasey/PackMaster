4014ef13d59e64d283e77642f52b928bebdc1147
casey <494294315@qq.com>
2021-12-09 16:53:50 +0800

    修复音效bug

---
+++ fixenv.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+chmod +x quick-3.5/tools/cocos2d-console/bin/cocos
+chmod +x quick-3.5/tools/cocos2d-console/plugins/plugin_luacompile/bin2103/32/lua/luajit-mac
+chmod +x quick-3.5/tools/cocos2d-console/plugins/plugin_luacompile/bin2103/64/lua/luajit-mac
